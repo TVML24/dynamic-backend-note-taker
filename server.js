7b2ccd23afb2b6c17446dcdc58fbb2ee656fd845
Tim <tvanmookletcher@gmail.com>
2023-03-01 20:29:00 +1030

    attempt at bugfix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const { clog } = require('./middleware/clog');
 const apiRoutes = require('./routes/index');
 
-const PORT = process.env.port || 3001;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
